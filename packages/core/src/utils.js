86b2945a24eef626e10564206c4bc01c5a111f30
marekb <marecica2@gmail.com>
2020-12-28 12:24:48 +0100

    Fix undefined formData in dependency

--- packages/core/src/utils.js
+++ packages/core/src/utils.js
@@ -748,7 +748,7 @@ function processDependencies(
   // Process dependencies updating the local schema properties as appropriate.
   for (const dependencyKey in dependencies) {
     // Skip this dependency if its trigger property is not present.
-    if (formData[dependencyKey] === undefined) {
+    if (formData == null || formData[dependencyKey] === undefined) {
       continue;
     }
     // Skip this dependency if it is not included in the schema (such as when dependencyKey is itself a hidden dependency.)
